60a2f7b077703938f0e3c9611a14829d81b3c750
jjuiddong <jjuiddong@hanmail.net>
2012-11-21 21:53:13 -0800

    first Network Library
    commit first Network Library

---
+++ Network/Network/Network.cpp
@@ -0,0 +1,16 @@
+
+#include "stdafx.h"
+#include "Network.h"
+
+
+void Network::Send()
+{
+
+}
+
+
+void Network::Recv()
+{
+
+}
+
---
+++ Network/Network/Network.h
@@ -0,0 +1,14 @@
+
+#ifndef __NETWORK_H__
+#define __NETWORK_H__
+
+
+namespace Network
+{
+
+	void Send();
+	void Recv();
+
+};
+
+#endif // __NETWORK_H__
