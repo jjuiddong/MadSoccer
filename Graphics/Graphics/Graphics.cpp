2a2bef11b10e345668fdc2165072725cce4f4b3d
jjuiddong <jjuiddong@hanmail.net>
2012-11-21 21:53:40 -0800

    first Graphics Library
    commit first Graphics Library

---
+++ Graphics/Graphics/Graphics.cpp
@@ -0,0 +1,9 @@
+
+#include "stdafx.h"
+#include "Graphics.h"
+
+
+void Graphics::Draw()
+{
+
+}
---
+++ Graphics/Graphics/Graphics.h
@@ -0,0 +1,12 @@
+
+#ifndef __GRAPHICS_H__
+#define __GRAPHICS_H__
+
+
+namespace Graphics
+{
+	void Draw();
+
+};
+
+#endif __GRAPHICS_H__
